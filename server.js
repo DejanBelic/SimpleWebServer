bf341bbb91e77c10d3f011851aee35ef5683ea86
Dejan <dejan.belic@develomon.com>
2019-04-06 22:36:05 +0200

    Simple web server completed.

--- server.js
+++ server.js
@@ -1,15 +1,48 @@
 const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
 
+const mimeTypes = {
+  "html": "text/html",
+  "jpeg": "image/jpeg",
+  "jpg": "image/jpg",
+  "png": "image/png",
+  "js": "text/javascript",
+  "css": "text/css"
+};
 
-const hostname = '127.0.0.1';
-const port = 3000;
+http.createServer((req, res) => {
+  let uri = url.parse(req.url).pathname;
+  let filename = path.join(process.cwd(), unescape(uri));
+  console.log('Loading '  + uri);
+  let stats;
 
-const server = http.createServer((req, res) => {
-  res.status = 200;
-  res.setHeader('Content-Type', 'text/plain ');
-  res.end('Hello World\n');
-});
+  try {
+    stats = fs.lstatSync(filename);
+  }
+  catch (e) {
+    res.writeHead(404, { 'Content-Type': 'text/plain'});
+    res.write('404 Not found');
+    res.end();
+    return;
+  }
+  if (stats.isFile()) {
+    let mimeType = mimeTypes[path.extname(filename).split('.').reverse()[0]];
+    res.writeHead(200, {'Content-Type': mimeType});
 
-server.listen(port, hostname, () => {
-  console.log(`Server running on http://${hostname}:${port}`)
-});
+    let fileStream = fs.createReadStream(filename);
+    fileStream.pipe(res);
+  }
+  else if (stats.isDirectory()) {
+    res.writeHead(302, {
+      'Location': 'index.html'
+    })
+    res.end();
+  }
+  else {
+    res.writeHead(500, {'Content-Type': 'text/plain'});
+    res.write('500 Internal Error');
+    res.end();
+  }
+}).listen(1337);
